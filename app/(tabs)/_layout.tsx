c08f3229cbc925a067c7b1eea10e9358987db672
AchabCode <Laurianseraphin@gmail.com>
2025-05-13 23:16:28 +0200

    Create preprod branch and update navigation text

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -52,7 +52,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="journal"
         options={{
-          title: 'Calculateur',
+          title: 'Simulateur',
           tabBarIcon: ({ color }) => <Calculator size={24} color={color} />,
         }}
       />
