cfeca66ee7792f5a1de308e86a3194c24387b522
AchabCode <Laurianseraphin@gmail.com>
2025-05-14 11:22:54 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Wallet, BarChart } from 'lucide-react-native';
+import { Chrome as Home, Wallet, ChartBar as BarChart } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
